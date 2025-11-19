03c41f46261a4d3283beea7eb3514e5b4edfbe2c
Nayanacoderc <nayanakgouda0306@gmail.com>
2025-11-19 12:21:15 +0530

    Create Sum of array.c

---
+++ Sum of array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a[1];
+    int sum=0;
+    printf("enter the elements ");
+    scanf("%d\n",&a);
+    for(int i=0;i<=a;i++){
+        scanf("%d\n",a[i]);
+    }
+}
